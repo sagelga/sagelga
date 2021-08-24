52207a9b1f86e1ba3aa463d3cf541dd2e337ce94
Kunanon Srisuntiroj <son9912@gmail.com>
2021-08-24 07:50:38 +0000

    Add about page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,14 +37,35 @@ module.exports = {
   themeConfig: {
     search: false,
     lastUpdated: 'Last Updated',
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Featured Projects', link: '/project/' },
-      { text: 'Working Experience', link: '/experience/' },
-      { text: 'Skills', link: '/skill/' },
-      { text: 'Education', link: '/education/' },
+    nav: [{
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'About',
+        link: '/about/'
+      },
+      {
+        text: 'Featured Projects',
+        link: '/project/'
+      },
+      {
+        text: 'Experience',
+        link: '/experience/'
+      },
+      {
+        text: 'Skills',
+        link: '/skill/'
+      },
+      {
+        text: 'Education',
+        link: '/education/'
+      },
       // { text: 'Interests', link: '/interests/' },
-      { text: 'Contact', link: '/contact/' },
+      {
+        text: 'Contact',
+        link: '/contact/'
+      },
     ]
   }
 
---
+++ docs/about/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroText: About
+tagline: ' '
+footer: Copyright © 2021 Kunanon Srisuntiroj
+---
+
