82f2fa25894d4da1e0d04dc6a164027d9404f1c4
Kunanon Srisuntiroj <son9912@gmail.com>
2022-01-24 09:32:57 +0000

    Change config file to .ts
    This will allows me to use same template with https://github.com/sagelga/documentation

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,58 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+import { navbar, sidebar } from './configs'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  title: 'This is me, sagelga',
+  description: 'Portfolio of me',
+
+  // head: [
+  //   ['link', {
+  //     rel: 'stylesheet',
+  //     href: 'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css'
+  //   }],
+  //   ['link', {
+  //     rel: 'stylesheet',
+  //     href: 'https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css'
+  //   }]
+  // ],
+
+  // https://v2.vuepress.vuejs.org/reference/default-theme/config.html
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    home: '/',
+
+    // logo: 'https://img.icons8.com/fluency/64/000000/layers.png',
+
+    navbar: navbar.en,
+    sidebar: sidebar.en,
+
+    lastUpdated: true,
+    lastUpdatedText: 'Last Update (UTC)',
+
+    editLink: true,
+    editLinkText: 'Report an Issue',
+
+    contributors: true,
+    contributorsText: 'Contributor(s)',
+
+    docsRepo: 'sagelga/sagelga',
+    docsDir: 'docs',
+    docsBranch: 'main',
+  },
+
+  plugins: [
+    ['@vuepress/plugin-google-analytics', {
+      id: 'G-BTF0LX3HTD'
+    }
+    ],
+    // ['@vuepress/plugin-search', {
+    //   locales: {
+    //     '/': {
+    //       placeholder: 'Search',
+    //     },
+    //   },
+    // },
+    // ],
+  ],
+})
