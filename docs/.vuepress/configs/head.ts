698e69b5350f5d6cd21225b8f9a9c5cc8b958f80
Kunanon Srisuntiroj <son9912@gmail.com>
2022-07-03 15:27:55 +0000

    Move header to independent file

---
+++ docs/.vuepress/configs/head.ts
@@ -0,0 +1,43 @@
+import type { HeadConfig } from "@vuepress/core";
+
+export const head: HeadConfig[] = [
+  [
+    "link",
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "https://img.icons8.com/fluency/16/000000/home.png",
+    },
+  ],
+  [
+    "link",
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "https://img.icons8.com/fluency/32/000000/home.png",
+    },
+  ],
+  [
+    "link",
+    {
+      rel: "stylesheet",
+      href: "https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css",
+    },
+  ],
+  [
+    "meta",
+    {
+      name: "google-site-verification",
+      content: "YfryvcUpXvpvzX8xdK2JY2yad89if-kCk1Mo7ogs5Is",
+    },
+  ],
+  [
+    "meta",
+    {
+      name: "apple-mobile-web-app-status-bar-style",
+      content: "black",
+    },
+  ],
+];
