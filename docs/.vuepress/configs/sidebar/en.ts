ddcbc967d2412cbd14ccc15df0921f75800005c8
Kunanon Srisuntiroj <son9912@gmail.com>
2023-08-07 10:26:31 +0700

    Format files with Prettier

--- docs/.vuepress/configs/sidebar/en.ts
+++ docs/.vuepress/configs/sidebar/en.ts
@@ -1,6 +1,6 @@
 // https://v2.vuepress.vuejs.org/reference/default-theme/config.html#sidebar
 // https://github.com/vuepress/vuepress-next/blob/main/docs/.vuepress/configs/sidebar/en.ts
 
-import type { SidebarConfig } from "@vuepress/theme-default";
+import type { SidebarConfig } from '@vuepress/theme-default';
 
 export const sidebarEn: SidebarConfig = {};
